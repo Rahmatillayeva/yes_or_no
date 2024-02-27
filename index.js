bc0e96e93943a3573137f55f8026fcf80deebdea
Rahmatillayeva <shaxrizodamrahmatillayeva@gmail.com>
2024-02-27 13:37:40 +0500

    yes/no

---
+++ index.js
@@ -0,0 +1,31 @@
+const elementForm = document.querySelector(".form");
+const elementInputTemperature = document.querySelector(".form-input-temperature");
+const elementInputRun = document.querySelector(".form-input-run");
+const elementInputGym = document.querySelector(".form-input-gym");
+const elementYes = document.querySelector(".main-heading-yes");
+const elementNo = document.querySelector(".main-heading-no");
+
+elementForm.addEventListener("submit", function(evn) {
+  evn.preventDefault();
+
+const inputValue = elementInputTemperature.value;
+const inputCheckboxValueRun = elementInputRun.checked;
+const inputCheckboxValueGym = elementInputGym.checked;
+
+  if (inputValue > 5 && inputValue < 30 ) {
+    elementYes.style.color = "green";
+    elementNo.style.color = "rgb(72, 14, 72)"
+  }
+  else if (inputValue < 5 && inputCheckboxValueGym == true) {
+    elementNo.style.color = "red";
+    elementYes.style.color = "rgb(72, 14, 72)";
+}
+  else if (inputCheckboxValueRun == true && inputCheckboxValueGym == true){
+    elementYes.style.color = "green";
+    elementNo.style.color = "rgb(72, 14, 72)"
+  }
+  else if (elementInputRun.checked == true) {
+    elementNo.style.color = "red";
+    elementYes.style.color = "rgb(72, 14, 72)";
+  }
+})
